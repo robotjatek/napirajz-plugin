389a7630ebdd4de5352e5e2eb76ffc8747e38028
Attila Szücs <ati.szucs@hotmail.com>
2020-06-07 13:15:45 +0200

    Fixed firefox addon page warnings

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # napirajz-plugin
+
+https://addons.mozilla.org/hu/firefox/addon/napirajz-plugin/
+
+Forked from https://github.com/spartalos/napirajz-plugin
--- newtab.js
+++ newtab.js
@@ -1,7 +1,7 @@
 /* Social Network Share */
 var randomButtonTexts = ['STÉG', 'ZAPPA', 'STUNK', 'KONGLON', 'HRANULÁK', 'TADÜN',
-'GLÓTAH', 'SKALÜNT', 'KREDALÁN', 'AMAKUN', 'KANDÁN', 'FLOCSK', 'FLASK', 'VROMPLON', 'VEGANÉZ'
-, 'UDUN', 'FLOKSZON', 'SZLOPAKIKKI', 'ROMMEL', 'RÖTÖPEM'];
+  'GLÓTAH', 'SKALÜNT', 'KREDALÁN', 'AMAKUN', 'KANDÁN', 'FLOCSK', 'FLASK', 'VROMPLON', 'VEGANÉZ'
+  , 'UDUN', 'FLOKSZON', 'SZLOPAKIKKI', 'ROMMEL', 'RÖTÖPEM'];
 
 var mailToUrl = 'mailto:?subject=Napirajz&body=';
 var mailToIconUrl = './mailto-logo.png';
@@ -15,10 +15,10 @@ var tumblrIconUrl = './tumblr-logo.png';
 var twitterShareUrl = 'https://twitter.com/intent/tweet?text=';
 var twitterIconUrl = './twitter-logo.png';
 
-function httpGet(url, callback){
+function httpGet(url, callback) {
   var xmlHttp = new XMLHttpRequest();
-  xmlHttp.onreadystatechange = function(){
-    if(xmlHttp.readyState == 4 && xmlHttp.status == 200){
+  xmlHttp.onreadystatechange = function () {
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
       callback(xmlHttp.responseText);
     }
   }
@@ -26,22 +26,22 @@ function httpGet(url, callback){
   xmlHttp.send(null);
 }
 
-function createImageElement(imageUrl){
-  var imgTag=document.createElement("img");
-      imgTag.setAttribute('src', imageUrl);
-      imgTag.setAttribute('alt', 'na');
-      imgTag.setAttribute('style', 'max-width: 100%;');
+function createImageElement(imageUrl) {
+  var imgTag = document.createElement("img");
+  imgTag.setAttribute('src', imageUrl);
+  imgTag.setAttribute('alt', 'na');
+  imgTag.setAttribute('style', 'max-width: 100%;');
   return imgTag;
 }
 
-function createAElement(href, imgTag){
+function createAElement(href, imgTag) {
   var aTag = document.createElement("a");
   aTag.setAttribute('href', href);
   aTag.appendChild(imgTag);
   return aTag;
 }
 
-function addToShareContainer(){
+function addToShareContainer() {
   var shareContainer = document.getElementById('shareContainer');
   shareContainer.innerHTML = '';
   var i;
@@ -50,7 +50,7 @@ function addToShareContainer(){
   }
 }
 
-function createShare(shareBaseUrl, iconUrl, href){
+function createShare(shareBaseUrl, iconUrl, href) {
   var shareTag = document.createElement("a");
   shareTag.setAttribute('href', shareBaseUrl + href);
   shareTag.setAttribute('target', '_blank');
@@ -61,13 +61,13 @@ function createShare(shareBaseUrl, iconUrl, href){
   return shareTag;
 }
 
-function createH2(text){
+function createH2(text) {
   var h2 = document.createElement('h2');
-  h2.innerHTML = text;
+  h2.appendChild(text);
   return h2;
 }
 
-function createRajz(parsedResponse){
+function createRajz(parsedResponse) {
   var rajz = getFirst(parsedResponse);
   var rajzUrl = encodeURIComponent(rajz.LapURL != '' ? rajz.LapURL : rajz.URL);
   document.getElementById('kepdiv').innerHTML = '';
@@ -75,38 +75,38 @@ function createRajz(parsedResponse){
   document.getElementById('cimDiv').innerHTML = '';
   document.getElementById('cimDiv').appendChild(createH2(rajz.Cim));
   addToShareContainer(
-              createShare(mailToUrl, mailToIconUrl, rajzUrl),
-              createShare(faceShareUrl, faceIconUrl, rajzUrl),
-              createShare(tumbrlShareUrl, tumblrIconUrl, rajzUrl),
-              createShare(twitterShareUrl, twitterIconUrl, rajzUrl)
-              );
+    createShare(mailToUrl, mailToIconUrl, rajzUrl),
+    createShare(faceShareUrl, faceIconUrl, rajzUrl),
+    createShare(tumbrlShareUrl, tumblrIconUrl, rajzUrl),
+    createShare(twitterShareUrl, twitterIconUrl, rajzUrl)
+  );
 }
 
-function getFormattedDate(){
+function getFormattedDate() {
   var today = new Date();
-  return today.getFullYear() + '-' + today.getMonth()+1 + '-' + today.getDate();
+  return today.getFullYear() + '-' + today.getMonth() + 1 + '-' + today.getDate();
 }
 
-function getRandom(){
+function getRandom() {
   httpGet('http://kereso.napirajz.hu/abort.php?guppi&json',
-          function(responseRand){
-            createRajz(JSON.parse(responseRand));
-          });
+    function (responseRand) {
+      createRajz(JSON.parse(responseRand));
+    });
 }
 
-function getFirst(responseJson){
-  for(pic in responseJson){
+function getFirst(responseJson) {
+  for (pic in responseJson) {
     return responseJson[pic];
   }
 }
 
-function setButtonText(button){
-  button.innerHTML = randomButtonTexts[Math.floor(Math.random()*randomButtonTexts.length)];
+function setButtonText(button) {
+  button.textContent = randomButtonTexts[Math.floor(Math.random() * randomButtonTexts.length)];
 }
 
-window.onload = function(){
+window.onload = function () {
   var randomButton = document.getElementById('randomDiv');
-  randomButton.onclick = function(){
+  randomButton.onclick = function () {
     getRandom();
     setButtonText(this);
   }
